6accfd257a4692b0024da3c89b744af0b759711b
RipeCherries <alexeyegoshin0403@mail.ru>
2023-02-05 20:19:26 +0700

    Create a config folder

--- .config/rollup.config.js
+++ .config/rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import babel from "rollup-plugin-babel";
-import pkg from "./package.json";
+import pkg from "../package.json";
 
 export default [
     {
