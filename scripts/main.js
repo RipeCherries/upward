41cc3044b3743729782f5729c33827504c180cc3
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-02-13 21:28:07 +0700

    Add go up button for demo

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+import UpWard from "@ripecherries/up-ward";
+
+const up = new UpWard({
+    position: "right",
+    offset: 30,
+    bottomOffset: 30,
+    containerSize: 50,
+    containerColor: "#17252A",
+    arrowColor: "#FEFFFF",
+    trigger: 800,
+    zIndex: 1
+});
